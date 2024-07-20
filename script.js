b83670c543c23e480466652f0811afdba3c590b5
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-07-20 09:47:51 +0000

    adding a signature " G for Grateful 🤍"

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const asciiArt = `
+ _    ____      _____                ____           _        __       _   _ 
+| |  / ___|    |  ___|__  _ __      / ___|_ __ __ _| |_ ___ / _|_   _| | | |
+| | | |  _     | |_ / _ \\| '__|    | |  _| '__/ _\` | __/ _ \\ |_| | | | | | |
+|_| | |_| |    |  _| (_) | |       | |_| | | | (_| | ||  __/  _| |_| | | |_|
+(_)  \\____|    |_|  \\___/|_|        \\____|_|  \\__,_|\\__\\___|_|  \\__,_|_| (_)
+                                    ♥
+`;
 
-
+console.log(asciiArt);
 /* Given the 'id' attribute of a form element, this will return the value entered by the user into that form element. */
 function formValue(id) {
   // Retrieve the form element with the specified id
